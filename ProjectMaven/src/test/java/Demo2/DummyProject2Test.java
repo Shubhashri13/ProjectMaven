aca16e4b711327ea049f6645eef06b4e4e6e99e3
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-06 19:31:23 +0530

    Amazon

---
+++ ProjectMaven/src/test/java/Demo2/DummyProject2Test.java
@@ -0,0 +1,14 @@
+package Demo2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DummyProject2Test
+{
+	@Test
+	public void amazon2()
+	{
+		Reporter.log("Amazon app",true);
+	}
+
+}
