0be7e1823def7ce462255656f890737ecf705337
Pawan <Pawan@LAPTOP-L8DJVMDE>
2025-02-06 19:13:31 +0530

    ProjectMaven

---
+++ ProjectMaven/src/test/java/Demo2/DummyProjectTest.java
@@ -0,0 +1,14 @@
+package Demo2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DummyProjectTest
+{
+	@Test
+	public void amazon()
+	{
+		Reporter.log("Application is launched",true);
+	}
+
+}
